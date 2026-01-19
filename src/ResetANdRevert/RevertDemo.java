c8779dd14ae512bc1bee23493755b8acc171e5d8
orozobekovtair <tairorozobekov@gmail.com>
2026-01-19 09:44:02 +0600

    Revert "demo3"
    This reverts commit f8de809847c9c6766d2510daf1625b271e1c2154.

--- src/ResetANdRevert/RevertDemo.java
+++ src/ResetANdRevert/RevertDemo.java
@@ -4,7 +4,6 @@ public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("demo1");
         System.out.println("demo2");
-        System.out.println(demo3);
 
     }
 }
