f8de809847c9c6766d2510daf1625b271e1c2154
orozobekovtair <tairorozobekov@gmail.com>
2026-01-19 09:35:26 +0600

    demo3

--- src/ResetANdRevert/RevertDemo.java
+++ src/ResetANdRevert/RevertDemo.java
@@ -4,6 +4,7 @@ public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("demo1");
         System.out.println("demo2");
+        System.out.println(demo3);
 
     }
 }
